f3a761a9ee66038320614f04578252473579329c
keerthi723 <keerthiga342@gmail.com>
2025-05-31 20:06:45 +0530

    bug fix
    fixing error

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Animated-Portfolio/",
   plugins: [react()],
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 731a8631739c82bc6ca9bc105023977e031e3e9d
