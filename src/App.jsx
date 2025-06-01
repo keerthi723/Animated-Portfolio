7a42ecc1763f4529a97dbf82fee872f66fa840c4
keerthi723 <keerthiga342@gmail.com>
2025-06-01 19:30:06 +0530

    error fix
    fixing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Components/Navbar";
-import "./app.scss"
+import "./App.scss";
 import Test from "./Test";
 import Hero from "./Components/hero/Hero";
 import Parallax from "./Components/parallax/Parallax";
@@ -8,19 +8,30 @@ import Portfolio from "./Components/portfolio/Portfolio";
 import Contact from "./Components/contact/Contact";
 import Cursor from "./Components/cursor/Cursor";
 const App = () => {
-  return <div>
-     <Cursor />
-    <section id="HomePage"><Navbar/>
-    <Hero/>
-    </section>
-    <section id="Services"><Parallax type="services"/></section>
-    <section id="Order"><Services /></section>
-    
-   <section id="Portfolio"><Parallax  type="portfolio"/></section>
-    <Portfolio  />
-    <section id="Contact"><Contact /></section>
-  {/* <Test /> */}
-  </div>;
+  return (
+    <div>
+      <Cursor />
+      <section id="HomePage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section id="Order">
+        <Services />
+      </section>
+
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* <Test /> */}
+    </div>
+  );
 };
 
 export default App;
